50e9ad1290bf1c6f27d38d8914aef4de632a62cc
z4ph0d <frank.michel.42@gmail.com>
2015-01-25 23:27:18 +0100

    updates
    best/worst still borked

--- Assigments/Assignment3/rankall.R
+++ Assigments/Assignment3/rankall.R
@@ -4,8 +4,13 @@ rankall <- function(outcome, num = "best") {
         states <- data[,7]
         
         statesu <- unique(states)
-        print(statesu)
+        #print(statesu)
         colNumber <- NULL
+        rank<-NULL
+        
+        if(num=="best")
+                rank<-1
+        
         
         
         ## Check that state and outcome are valid
@@ -26,15 +31,43 @@ rankall <- function(outcome, num = "best") {
         
         ## order data by state and outcome
         ordereddata <- cleandata[order(cleandata[,7], cleandata[,colNumber]),]
-        print(head(ordereddata[,c(7,colNumber)]))
+        #print(head(ordereddata[,c(7,colNumber)]))
         
         splitod <- split(ordereddata, ordereddata$State)
-        print(head(splitod[[2]][,7]))
-        
+
+        #res<-lapply(splitod, function(x) tail(x[,c(2,7)],1))
+        res<-(lapply(splitod, getResult, num))
+        #return(res)
+        #####
+        #return(unsplit(res,statesu))
+        result<-data.frame(matrix(unlist(res), nrow=length(statesu), byrow=T))
+        colnames(result)<-c("hospital", "state")
+        return(result)
+        #print(res)
+        #result<-as.data.frame(res)
+        #print(result)
         
         ## For each state, find the hospital of the given rank
         
         
         ## Return a data frame with the hospital names and the
         ## (abbreviated) state name
+}
+getResult<-function(x, num) {
+        if(num=="best" && length(x[,1])>0){
+                print(paste(num,length(x[,1])))
+                return(x[1,c(2,7)])    
+        }
+        else if(num=="worst"&& length(x[,1])>0){
+                print(paste(num,length(x[,1])))
+                return(x[length(x[,1]),c(2,7)])
+        }
+        else if(as.numeric(num<=length(x[,1]))){
+                print(paste(num,length(x[,1])))
+                return(x[as.numeric(num),c(2,7)])
+        }
+        else{
+                print(num)
+                return(list("NA", x[1,7]))
+        }
 }
